0238fe9d2dd0c79dc1882746bdb486601f0f303c
pranav370 <pranav@tungstonlabs.com>
2025-07-30 16:36:07 +0530

    `Update server logging to use process.env.PORT instead of hardcoded PORT`

--- Server/index.js
+++ Server/index.js
@@ -12,5 +12,5 @@ app.use("/api", contactRoutes);
 
 // const PORT = 5000;
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
